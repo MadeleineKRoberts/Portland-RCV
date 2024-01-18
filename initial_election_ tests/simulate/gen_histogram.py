396cfb5e22073689633e5b42b033d355d6f8d34f
Monica Nimmagadda <monicanimmagadda@Monicas-MacBook-Pro.local>
2024-01-17 21:36:33 -0600

    1000 simulation

--- initial_election_ tests/simulate/gen_histogram.py
+++ initial_election_ tests/simulate/gen_histogram.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import os 
 ballot_generators = {
     "bt": "Bradley Terry",
     "pl": "Plackett Luce",
@@ -8,7 +8,7 @@ ballot_generators = {
     #"ac": AlternatingCrossover,
 }
 
-def generate_histogram(data, election_type):
+def generate_histogram(data, election_type, simulation_type, params):
     unique_values, counts = np.unique(data, return_counts=True)
 
     plt.bar(unique_values, counts, align='center', alpha=0.7, width=0.8)
@@ -17,4 +17,8 @@ def generate_histogram(data, election_type):
     plt.xlabel('Number of Elected POC Candidates')
     plt.ylabel('Frequency')
     plt.title('Histogram for ' + ballot_generators[election_type] + ' model ')
+    current_directory = os.getcwd()
+    output_path = os.path.join(current_directory, 'Histograms', f'{election_type}_{simulation_type}_histogram.png')
+    plt.savefig(output_path)
+    plt.text(0.05, 0.95, params, transform=plt.gca().transAxes, fontsize=8, verticalalignment='top')
     plt.show()
